195a60a7d7fffc9a49bc57582993dec18d33ed1d
ainchoa <82274274+ainchoa@users.noreply.github.com>
2021-10-25 15:52:24 +0300

    idk

--- runners_book/_build/jupyter_execute/blog_notebook_test.py
+++ runners_book/_build/jupyter_execute/blog_notebook_test.py
@@ -8,7 +8,6 @@
 # In[1]:
 
 
-#hide
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -23,7 +22,6 @@ olympic = pd.read_csv('athlete_events.csv')
 # In[2]:
 
 
-#hide
 hundred_m = olympic[olympic['Event'] == "Athletics Men's 100 metres"]
 hundred_f = olympic[olympic['Event'] == "Athletics Women's 100 metres"]
 
@@ -52,7 +50,6 @@ marathon_f = olympic[olympic['Event'] == "Athletics Men's Marathon"]
 # In[3]:
 
 
-#hide
 runners_all = pd.concat([hundred_m, hundred_f, two_h_m, two_h_f, four_h_m, four_h_f, eight_h_m, eight_h_f, oneandhalfk_m, oneandhalfk_f, fivek_m, fivek_f, tenk_m, tenk_f, marathon_m, marathon_f])
 
 useful_columns = ['ID', 'Sex', 'Age', 'Height', 'Weight', 'Year', 'Event', 'Medal']
@@ -65,14 +62,12 @@ runners = runners[runners['Year'] > 1980]
 # In[4]:
 
 
-#hide
 numeric_dict = {'F' : 0, 'M' : 1, "Athletics Men's 100 metres" : 100, "Athletics Women's 100 metres" : 100, "Athletics Men's 200 metres" : 200,                 "Athletics Women's 200 metres" : 200, "Athletics Men's 400 metres" : 400, "Athletics Women's 400 metres" : 400, "Athletics Men's 800 metres" : 800,                 "Athletics Women's 800 metres" : 800, "Athletics Men's 1,500 metres" : 1500, "Athletics Women's 1,500 metres" : 1500,  "Athletics Women's 5,000 metres" : 5000,                 "Athletics Men's 5,000 metres" : 5000, "Athletics Women's 10,000 metres" : 10000, "Athletics Men's 10,000 metres" : 10000,                 "Athletics Women's Marathon" : 42195, "Athletics Men's Marathon" : 42195, 'Bronze' : 2, 'Silver' : 3, 'Gold' : 4, 'NaN' : 1}
 
 
 # In[5]:
 
 
-#hide
 r_numeric = runners.replace({"Sex": numeric_dict, 'Event': numeric_dict, 'Medal': numeric_dict})
 
 r_numeric['Medal'] = r_numeric['Medal'].fillna(1)
@@ -81,11 +76,10 @@ r_numeric['Medal'] = r_numeric['Medal'].fillna(1)
 # In[6]:
 
 
-#hide
 r_numeric.dropna(inplace = True)
 
 
-# We looked at the data from actual Olympic athletes so you don't have to:
+# We looked at the data from actual Olympic athletes so you don't have to, and here's how it looked like:
 
 # In[7]:
 
@@ -99,3 +93,5 @@ sns.regplot(x='Height', y='Weight', data=r_numeric[r_numeric['Sex'] == 1])
 
 sns.regplot(x='Height', y='Weight', data=r_numeric[r_numeric['Sex'] == 0])
 
+
+# But worry not, we've got you. Let's unpack this and make it useful for you.
